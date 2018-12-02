79b9af728cada5438c31d9278d0a54b70f166e83
bry-an <bbyunis@gmail.com>
2018-12-02 13:06:23 -0700

    testing addAnswer mutation occurring on radio check

--- src/test/unit/SurveyList.spec.js
+++ src/test/unit/SurveyList.spec.js
@@ -1,4 +1,4 @@
-import { shallowMount, createLocalVue } from "@vue/test-utils"
+import { mount, shallowMount, createLocalVue } from "@vue/test-utils"
 import SurveyList from "../../components/SurveyList"
 import SurveyQuestion from "../../components/SurveyQuestion"
 import Vuex from "Vuex"
@@ -12,25 +12,42 @@ localVue.use(Vuex)
 
 describe("SurveyList", () => {
     let state
+    let mutations
 
 
     const build = () => {
         const wrapper = shallowMount(SurveyList, {
             localVue, 
             store: new Vuex.Store({
-                state
+                state,
+                mutations
+            })
+        })
+        const wrapperQuestion = shallowMount(SurveyQuestion, {
+            localVue, 
+            store: new Vuex.Store({
+                state, 
+                mutations
+
             })
         })
         return {
             wrapper,
+            wrapperQuestion,
             SurveyQuestion: () => wrapper.find(SurveyQuestion),
             button: () => wrapper.find("#result")
         }
+        
     }
+
+
     beforeEach(() => {
-        state = {}
         state = {...store.state}
-        state.userAnswers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        mutations = {
+            addAnswer: jest.fn()
+        }
+
+        // state.userAnswers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     })
 
     it("is a Vue instance", () => {
@@ -44,12 +61,17 @@ describe("SurveyList", () => {
         expect(SurveyQuestion().exists()).toBe(true)
     })
 
-    it("Renders complete button if showButton is true", () => {
-        const { wrapper } = build()
-        const { button } = build()
-        console.info('state', state.userAnswers)
+    // it("Renders complete button if all answers checked", () => {
+    //     const { button } = build()
 
+    //     expect(button().exists()).toBe(true)
+    // })
 
-        expect(button().exists()).toBe(true)
+    it("increments user answer if an answer button checked", () => {
+        const { wrapperQuestion } = build()
+
+        const radio = wrapperQuestion.find("input[type='radio']")
+        radio.setChecked()
+        expect(mutations.addAnswer).toHaveBeenCalled()
     })
 })
